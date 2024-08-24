b76ad3d4ca3e765e1dd4fcf2438b8f0e2596c0fb
al-martyn1 <amart@mail.ru>
2024-08-24 22:20:13 +0300

    Добавил генерацию брифов

--- gen-doc.bat
+++ gen-doc.bat
@@ -1,1 +1,8 @@
-umba-md-pp --overwrite --batch-scan-recurse=.
+@call "%~dp0\make_sources_brief.bat"
+
+@set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,_distr_conf,src,.msvc2019,boost,icons
+@set COMMON=--overwrite --copy-images
+@set GRAPHVIZ_OPTIONS="--graphviz-output-path=%~dp0\..\.doc.generated_images"
+@rem set TEMP_OPTS=--graphviz-keep-temp-dot-files
+@set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE_DIRS% %GRAPHVIZ_OPTIONS% "--batch-scan-recurse=%~dp0\.."
+umba-md-pp %OPTIONS%
---
+++ make_sources_brief.bat
@@ -0,0 +1,5 @@
+@if exist "%~dp0\..\umba-brief-scanner.rsp" @set RSP=%~dp0\..\umba-brief-scanner.rsp
+umba-brief-scanner --where
+umba-brief-scanner "@%RSP%"        "--scan=%~dp0\../src"  "%~dp0\doc/_sources_brief.txt"
+umba-brief-scanner "@%RSP%" --html "--scan=%~dp0\../src"  "%~dp0\doc/_sources_brief.html"
+umba-brief-scanner "@%RSP%" --md   "--scan=%~dp0\../src"  "%~dp0\doc/_sources_brief.md_"
