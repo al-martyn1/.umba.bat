487cfc45ab121e379c141213f234ae142b6b3575
al-martyn1 <amart@mail.ru>
2024-08-27 02:36:56 +0300

    Вкорячиваем поддержку MSVC2017

---
+++ cmake_build_all_msvc2017.bat
@@ -0,0 +1,2 @@
+@call "%~dp0\_call_cmake_msvc_impl.bat" msvc2017 x86 GENERATE && @call "%~dp0\_call_cmake_msvc_impl.bat" msvc2017 x64 GENERATE && @call "%~dp0\_call_cmake_msvc_impl.bat" msvc2017 x86 BUILD && @call "%~dp0\_call_cmake_msvc_impl.bat" msvc2017 x64 BUILD
+
