6e700edc8ef1b0e46657b66fb06b90fa493835db
al-martyn1 <amart@mail.ru>
2024-09-13 23:50:25 +0300

    Вернул вывод команд гита

--- update_all_impl.bat
+++ update_all_impl.bat
@@ -1,10 +1,10 @@
 @cd "%~dp0.."
 @echo Pulling project
-@git pull --recurse-submodules
+git pull --recurse-submodules
 @if not exist "%~dp0..\.gitmodules" @goto SUBMODULES_UPDATE_DONE
 @echo Updating submodules
 @cd "%~dp0.."
-@git submodule update --init --recursive --remote --merge
+git submodule update --init --recursive --remote --merge
 @if "%1"=="no-pull" @goto NO_PULL
 @if "%1"=="no_pull" @goto NO_PULL
 @if "%1"=="NO-PULL" @goto NO_PULL
@@ -16,13 +16,13 @@
 @if not exist "%~dp0..\.bat" @goto SUBMODULE_BAT_DONE
 @cd "%~dp0..\.bat"
 @echo Pulling .bat submodule
-@git pull
+git pull
 @cd "%~dp0.."
 :SUBMODULE_BAT_DONE
 @if not exist "%~dp0..\.cmake" @goto SUBMODULE_CMAKE_DONE
 @cd "%~dp0..\.cmake"
 @echo Pulling .cmake submodule
-@git pull
+git pull
 @cd "%~dp0.."
 :SUBMODULE_CMAKE_DONE
 :NO_PULL
