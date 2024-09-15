1ab702804bb87de355d62f9c28b20918c5839bc0
al-martyn1 <amart@mail.ru>
2024-09-16 00:47:50 +0300

    Обновил скрипты

--- gen-doc.bat
+++ gen-doc.bat
@@ -4,5 +4,5 @@
 @set COMMON=--overwrite --copy-images
 @set GRAPHVIZ_OPTIONS="--graphviz-output-path=%~dp0\..\doc\.generated_images"
 @rem set TEMP_OPTS=--graphviz-keep-temp-dot-files
-@set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE_DIRS% %GRAPHVIZ_OPTIONS% "--batch-scan-recurse=%~dp0\.."
+@set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE_DIRS% %GRAPHVIZ_OPTIONS% %UMBA_MDPP_EXTRA_OPTIONS% "--batch-scan-recurse=%~dp0\.."
 umba-md-pp %OPTIONS%
