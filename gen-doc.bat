fcbc9a78defebd6675f0eed73bc91f3460ca4371
al-martyn1 <amart@mail.ru>
2024-09-16 16:22:15 +0300

    Исключил каталог doxy из поиска сорцов. Добавил поиск сорцов в подкаталоге examples

--- gen-doc.bat
+++ gen-doc.bat
@@ -1,6 +1,6 @@
 @call "%~dp0\make_sources_brief.bat"
 
-@set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,conf,_distr_conf,src,.msvc2019,boost,icons
+@set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,conf,_distr_conf,doxy,src,.msvc2019,boost,icons
 @set COMMON=--overwrite --copy-images
 @set GRAPHVIZ_OPTIONS="--graphviz-output-path=%~dp0\..\doc\.generated_images"
 @rem set TEMP_OPTS=--graphviz-keep-temp-dot-files
--- make_sources_brief.bat
+++ make_sources_brief.bat
@@ -1,6 +1,6 @@
 @if exist "%~dp0..\umba-brief-scanner.rsp" @set RSP="@%~dp0..\umba-brief-scanner.rsp"
 @rem echo RSP="@%RSP%"
 umba-brief-scanner --where
-umba-brief-scanner "%RSP%"        "--scan=%~dp0\../src"  "%~dp0\../doc/_sources_brief.txt"
-umba-brief-scanner "%RSP%" --html "--scan=%~dp0\../src"  "%~dp0\../doc/_sources_brief.html"
-umba-brief-scanner "%RSP%" --md   "--scan=%~dp0\../src"  "%~dp0\../doc/_sources_brief.md_"
+umba-brief-scanner --overwrite "%RSP%"        "--scan=%~dp0\../src" "--scan=%~dp0\../examples" "%~dp0\../doc/_sources_brief.txt"
+umba-brief-scanner --overwrite "%RSP%" --html "--scan=%~dp0\../src" "--scan=%~dp0\../examples" "%~dp0\../doc/_sources_brief.html"
+umba-brief-scanner --overwrite "%RSP%" --md   "--scan=%~dp0\../src" "--scan=%~dp0\../examples" "%~dp0\../doc/_sources_brief.md_"
