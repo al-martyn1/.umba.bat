b3fecbecd407fdc087d36b83a48f7ac35eb67d00
al-martyn1 <amart@mail.ru>
2024-09-25 23:13:56 +0300

    Заэкранировал инструкции в батнике

--- make-cli-options-docs.bat
+++ make-cli-options-docs.bat
@@ -1,16 +1,16 @@
-if not exist "%~dp0..\set_sln_exes.bat" @goto DO_NOTHING
-call "%~dp0..\set_sln_exes.bat"
+@if not exist "%~dp0..\set_sln_exes.bat" @goto DO_NOTHING
+@call "%~dp0..\set_sln_exes.bat"
 @rem
-call "%~dp0\setup_out_root_paths.bat"
+@call "%~dp0\setup_out_root_paths.bat"
 @rem
-IF "%OUTROOTPATH%"=="" (
+@IF "%OUTROOTPATH%"=="" @(
     echo OUTROOTPATH not found
     exit /B 1
 )
 @rem
-for %%i in (%SLN_EXES%) do (
+@for %%i in (%SLN_EXES%) do @(
      if not exist "%~dp0..\doc" mkdir "%~dp0..\doc"
      "%OUTROOTPATH%\Release\%%~i.exe" --md --help > "%~dp0..\doc\%%~i-cli-options.md_
 )
 :DO_NOTHING
-exit /B 0
+@exit /B 0
