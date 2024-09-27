1175a3211d4bd5ac4df39a0af301e41cdcb86288
al-martyn1 <amart@mail.ru>
2024-09-27 20:01:28 +0300

    Убрал настройку пути генерации изображений

--- gen-doc.bat
+++ gen-doc.bat
@@ -2,7 +2,10 @@
 
 @set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,conf,_distr_conf,doxy,src,.msvc2019,boost,icons
 @set COMMON=--overwrite --copy-images
-@set GRAPHVIZ_OPTIONS="--graphviz-output-path=%~dp0\..\doc\.generated_images"
+@rem 
+@set GRAPHVIZ_OPTIONS=
+@rem "--graphviz-output-path=%~dp0\..\doc\.generated_images"
+@rem
 @rem set TEMP_OPTS=--graphviz-keep-temp-dot-files
 @set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE_DIRS% %GRAPHVIZ_OPTIONS% %UMBA_MDPP_EXTRA_OPTIONS% "--batch-scan-recurse=%~dp0\.."
 umba-md-pp %OPTIONS%
