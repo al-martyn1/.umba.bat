18541152347a075da9a580e84eb2edb676df1231
al-martyn1 <amart@mail.ru>
2025-01-21 22:00:34 +0300

    Обновил генерацию брифов

--- make_sources_brief.bat
+++ make_sources_brief.bat
@@ -12,7 +12,7 @@
 @rem if "%BRIEF_EXTRA_OPTS_TXT_UNQUOTED%"=="" @set BRIEF_EXTRA_OPTS_TXT=--scan-notes "--notes-output-path=%~dp0\../doc/_md"
 @if "%BRIEF_EXTRA_OPTS_MD_UNQUOTED%"==""  @set BRIEF_EXTRA_OPTS_MD=--scan-notes "--notes-output-path=%~dp0\../doc/_md"
 umba-brief-scanner --where
-@rem umba-brief-scanner --overwrite %RSP%         %BRIEF_SCAN_PATHS% %BRIEF_EXTRA_OPTS_TXT% "%~dp0\../doc/_sources_brief.txt"
-@rem umba-brief-scanner --overwrite %RSP% --html  %BRIEF_SCAN_PATHS%                        "%~dp0\../doc/_sources_brief.html"
-umba-brief-scanner --overwrite %RSP% --md         %BRIEF_SCAN_PATHS% %BRIEF_EXTRA_OPTS_MD%  "%~dp0\../doc/_md/sources_brief.md_"
-umba-brief-scanner --overwrite %RSP% --md --main  %BRIEF_SCAN_PATHS%                        "%~dp0\../doc/_md/entry_points.md_"
+@rem umba-brief-scanner --overwrite %RSP%                %BRIEF_SCAN_PATHS% %BRIEF_EXTRA_OPTS_TXT% "%~dp0\../doc/_md/_sources_brief.txt"
+@rem umba-brief-scanner --overwrite %RSP% --html         %BRIEF_SCAN_PATHS%                        "%~dp0\../doc/_md/_sources_brief.html"
+umba-brief-scanner --overwrite %RSP% --md --split-group  %BRIEF_SCAN_PATHS% %BRIEF_EXTRA_OPTS_MD%  "%~dp0\../doc/_md/sources_brief.md_"
+umba-brief-scanner --overwrite %RSP% --md --main-only    %BRIEF_SCAN_PATHS%                        "%~dp0\../doc/_md/entry_points.md_"
