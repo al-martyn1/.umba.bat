156d5781362c3cd2b731a367f23ee42ddbe64fe0
al-martyn1 <amart@mail.ru>
2025-01-28 10:18:00 +0300

    Обновил (пофиксил) поиск EXE-шников в выходном каталоге

--- find-exe-in-out.bat
+++ find-exe-in-out.bat
@@ -2,11 +2,16 @@
 @set LOOKUP_FOR=%1
 @rem https://superuser.com/questions/1569197/what-is-the-meaning-of-tilde-in-batch-variables
 @FOR %%i IN (%OUTROOTPATHS%) DO @(
-    @FOR %%j IN (Debug Release) DO @(
-        @rem echo %%~i\%%j
-        @IF EXIST "%%~i\%%j\%LOOKUP_FOR%.exe" @(
-            @set FOUND_EXE="%%~i\%%j\%LOOKUP_FOR%.exe"
-            @goto FOUND
+    @rem echo I=%%~i
+    @FOR %%p in (%PLATFORMS%) do @(
+        @rem echo P=%%~i
+        @FOR %%j IN (Debug Release) DO @(
+            @rem echo J=%%j
+            @rem echo TEST_PATH=%~dp0\..\.out\%%~i\%%p\%%j\%LOOKUP_FOR%.exe
+            @IF EXIST "%~dp0\..\.out\%%~i\%%p\%%j\%LOOKUP_FOR%.exe" @(
+                @set FOUND_EXE="%~dp0\..\.out\%%~i\%%p\%%j\%LOOKUP_FOR%.exe"
+                @goto FOUND
+)
 )
 )
 )
