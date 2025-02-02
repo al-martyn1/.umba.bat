efbbe5f805540f421dc9bf388fdca4905683bb65
al-martyn1 <amart@mail.ru>
2025-02-02 21:04:27 +0300

    Добавил checkout ветки main в модулях .bat, .cmake

--- update_all_impl.bat
+++ update_all_impl.bat
@@ -16,12 +16,14 @@ git submodule update --init --recursive --merge
 @if not exist "%~dp0..\.bat" @goto SUBMODULE_BAT_DONE
 @cd "%~dp0..\.bat"
 @echo Pulling .bat submodule
+git.exe checkout main --
 git pull
 @cd "%~dp0.."
 :SUBMODULE_BAT_DONE
 @if not exist "%~dp0..\.cmake" @goto SUBMODULE_CMAKE_DONE
 @cd "%~dp0..\.cmake"
 @echo Pulling .cmake submodule
+git.exe checkout main --
 git pull
 @cd "%~dp0.."
 :SUBMODULES_UPDATE_DONE
